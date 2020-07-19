1aa088f6b76fdf2a0716446613866793fe1d2dbe
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2020-07-19 22:42:57 +0700

    form

--- application/views/article/vform.php
+++ application/views/article/vform.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>    
+  <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
   </head>
 
   <body>
@@ -12,40 +13,40 @@
                 <form method="POST" action="" enctype="multipart/form-data">
                     <div class="form-group" >
                         <label class="col-sm-4 form-control-label"> Judul Artikel:</label>  
-                        <input type="text" class="form-control "  placeholder="Judul Artikel">                                                                        
+                        <input type="text" class="form-control " name="judul" placeholder="Judul Artikel">                                                                        
                     </div>
                     <div class="form-group">
                         <label class="col-sm-4 form-control-label"> Jenis Artikel:</label>  
-                        <select class="custom-select mb-3 required" >
+                        <select class="custom-select mb-3 required" name="jenis_artikel">
                         <option disabled selected>Pilih Jenis Artikel</option>
-                        <option value="">Feature</option>
-                        <option value="">Hype</option>
-                        <option value="">Review</option>
-                        <option value="">Music Info</option>
+                        <option value="1">Feature</option>
+                        <option value="2">Hype</option>
+                        <option value="3">Review</option>
+                        <option value="4">Music Info</option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label class="col-sm-4 form-control-label" >Gambar 1 :</label>
-                        <input type="file" class="form-control"  >  
+                        <input type="file" class="form-control" name="foto1" >  
                     </div>
                     <div class="form-group">
                         <label class="col-sm-4 form-control-label" >Gambar 2 :</label>
-                        <input type="file" class="form-control"  >  
+                        <input type="file" class="form-control" name="foto2" >  
                     </div>
                     <div class="form-group">
                         <label class="col-sm-4 form-control-label" >Gambar 3 :</label>
-                        <input type="file" class="form-control"  >  
+                        <input type="file" class="form-control" name="foto3" >  
                     </div>                                                          
                     <div class="form-groupo">
                         <label class="col-sm-4 form-control-label" >Isi Artikel :</label>
-                        <textarea id="mytextarea">Artikel</textarea>        
+                        <textarea id="mytextarea" name="essay"></textarea>        
                                           
                     </div>  
                     <div class="text-right">
                     <!-- <input type="button" class="btn btn-warning" onclick="history.back();" value="Batal"> -->
                     <button type="button" class="btn btn-secondary" onclick="history.back();">Batal</button>                    
                     <input type="text" hidden id="submitPresence" name="submitPresence" value="submitPresence">
-                    <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Simpan</button>
+                    <button type="submit" class="btn btn-success"> Simpan</button>
                     </div>
                 </form>
             </div>      
