4188e896ca89273596901d840eb1afdb6a0c8ee6
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2020-08-19 00:36:49 +0700

    1 2

--- application/views/article/vArticle.php
+++ application/views/article/vArticle.php
@@ -25,7 +25,7 @@
     		    <div class="card profile-card-5 text-black">
                     <a href="<?php echo base_url('article/view/').$art->id_artikel ?>">
                     <div class="card-img-block">
-    		            <img class="card-img-tops" src="<?php echo base_url('assets/upload/'.$art->foto1) ?>" style=" height: 550px;" alt="Card image cap">
+    		            <img class="card-img-tops" src="<?php echo base_url('assets/upload/'.$art->foto1) ?>"  alt="Card image cap">
     		            </div>
                     <div class="card-body pt-0">
                     <h5 class="card-title" title="<?php echo $art->judul?>"><?php echo substr( $art->judul, 0, 50) . '...'; ?></h5></a>
